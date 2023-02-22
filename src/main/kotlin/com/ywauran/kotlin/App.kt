0832d2441a067a460b07ca8b195b4f5dec1559f4
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-02-23 07:04:37 +0800

    add kotlin collections

--- src/main/kotlin/com/ywauran/kotlin/App.kt
+++ src/main/kotlin/com/ywauran/kotlin/App.kt
@@ -17,14 +17,60 @@ data class DataUser(val name: String, val age: Int) {
 }
 
 fun main(args: Array<String>) {
-    val user = User("Harke", 21)
-    val dataUser = DataUser("Harke", 21)
+//    val user = User("Harke", 21)
+//    val dataUser = DataUser("Harke", 21)
+//
+//    println(user)
+//    println(dataUser)
+//
+//    val ( name, age ) = dataUser
+//    println("Perkenalkan nama saya $name")
+//
+//    dataUser.intro()
 
-    println(user)
-    println(dataUser)
+    //List
+    val numberList : List<Int> = listOf(1, 2, 3, 4, 5)
+    println(numberList)
+    println(numberList[4])
+    val anyList = mutableListOf(1, "A", "Kotlin", true)
+    anyList.add(4, false)
+    anyList.add(5, "Programming")
+    println(anyList)
 
-    val ( name, age ) = dataUser
-    println("Perkenalkan nama saya $name")
+    //Set
+    val integerSet = setOf(1, 2, 3 ,4, 1, 5)
+    println(integerSet)
 
-    dataUser.intro()
+    val setA = setOf(1, 2, 3, 4)
+    val setB = setOf(3, 4, 2, 1)
+
+    println(setA == setB)
+    println(5 in setA)
+
+    val mutableSet = mutableSetOf(1, 2 ,3 , 3, 4, 4)
+
+//    mutableSet[2] = 6
+    mutableSet.add(6)
+    mutableSet.remove(3)
+    println(mutableSet)
+
+    //Map
+    val capital = mapOf(
+        "Jakarta" to "Indonesia",
+        "London" to "England",
+        "New Delhi" to "India"
+    )
+
+    println(capital["Jakarta"])
+    println(capital["Kuala Lumpur"])
+//    println(capital.getValue("Kuala Lumpur"))
+
+    println(capital.keys)
+    println(capital.values)
+
+    val mutableCapital = capital.toMutableMap()
+
+    mutableCapital.put("Kuala Lumpur", "Malaysia")
+
+    println(mutableCapital)
 }
